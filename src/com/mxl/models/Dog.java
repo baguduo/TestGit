7258995ac45bb6f9982f7fad7c688ddc196d2499
baguduo <568143335@qq.com>
2016-11-17 21:46:18 +0800

    second commit

--- src/com/mxl/models/Dog.java
+++ src/com/mxl/models/Dog.java
@@ -4,11 +4,7 @@ import com.mxl.interfaces.Being;
 
 public class Dog implements Being {
 	
-	private String msg;
 
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
 
 	
 	public void hobby() {
